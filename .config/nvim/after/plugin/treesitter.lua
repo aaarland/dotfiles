aa0e012673baa9c4e1e18edd09f79d3282116d78
Adrian Aarland <aaarland@reachmedianetwork.com>
2023-03-31 13:34:18 -0500

    Added work configs

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "help", "javascript", "typescript", "rust" },
+  -- A list of parser names, or "all"
+  ensure_installed = { "help", "javascript", "typescript", "json"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -10,8 +10,8 @@ require'nvim-treesitter.configs'.setup {
   auto_install = true,
 
   highlight = {
+    -- `false` will disable the whole extension
     enable = true,
-
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
@@ -19,22 +19,5 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
-  rainbow = {
-    enable = true,
-    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    colors = { "#1cecc4",
-    "#f7aa53",
-    "#3b2c1c",
-    "#6452ad",
-    "#b6c8d4",
-    "#444c"}, -- table of hex strings
-    termcolors = { "#1cecc4",
-    "#f7aa53",
-    "#3b2c1c",
-    "#6452ad",
-    "#b6c8d4",
-    "#444c"} -- table of colour name strings
-  }
 }
+
