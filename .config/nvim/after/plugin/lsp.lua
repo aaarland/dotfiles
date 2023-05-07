8219fe1d954f0bc4d33972e806533e830cfe66b2
Adrian <adrian@aswaa.dev>
2023-05-07 13:26:09 -0500

    :-^)

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -30,8 +30,8 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 })
 
 -- Use for github copilot
--- cmp_mappings['<Tab>'] = nil
--- cmp_mappings['<S-Tab>'] = nil
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
