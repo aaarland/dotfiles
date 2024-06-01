44ceea3f080e9ebb9aea95b5e214da336525b27a
Aarland <adrian.aarland@gmail.com>
2024-06-01 14:25:49 -0500

    feat: changed keybinds

--- .config/nvim/lua/aarland/lazy/harpoon.lua
+++ .config/nvim/lua/aarland/lazy/harpoon.lua
@@ -10,9 +10,9 @@ return {
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<C-a>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-d>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-f>", function() harpoon:list():select(5) end)
+        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<C-m>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<C-l>", function() harpoon:list():select(5) end)
     end
 }
