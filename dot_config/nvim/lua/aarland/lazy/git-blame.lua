8793d7ac68aee1f6dbab7c55c0bcc94f6ca1095e
Adrian Aarland <aaarland@reachmedianetwork.com>
2024-10-23 19:03:16 -0500

    feat: reverted git blame text column

--- dot_config/nvim/lua/aarland/lazy/git-blame.lua
+++ dot_config/nvim/lua/aarland/lazy/git-blame.lua
@@ -5,6 +5,6 @@ return {
         enabled = true, -- if you want to enable the plugin
         message_template = " <author> • <date> • <summary> • <<sha>>",
         date_format = "%r",
-        virtual_text_column = 80,
+        virtual_text_column = 1,
     }
 }
