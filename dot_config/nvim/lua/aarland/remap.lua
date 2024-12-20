63c5db292acea485a3de688069cde5afc086c1c8
Adrian Aarland <aaarland@reachmedianetwork.com>
2024-12-20 12:07:50 -0600

    feat: added oil

--- dot_config/nvim/lua/aarland/remap.lua
+++ dot_config/nvim/lua/aarland/remap.lua
@@ -37,3 +37,5 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open Parent Directory" })
