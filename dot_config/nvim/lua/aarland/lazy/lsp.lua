d8509a792dce2b2c443232e4167a1039d8158a22
Adrian Aarland <aaarland@reachmedianetwork.com>
2024-12-19 19:58:41 -0600

    feat: changed completions

--- dot_config/nvim/lua/aarland/lazy/lsp.lua
+++ dot_config/nvim/lua/aarland/lazy/lsp.lua
@@ -3,27 +3,13 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "rafamadriz/friendly-snippets",
-        "saadparwaiz1/cmp_luasnip",
+        "saghen/blink.cmp",
         "j-hui/fidget.nvim",
         "folke/neoconf.nvim",
     },
 
     config = function()
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
-        capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
+        local capabilities = require("blink.cmp").get_lsp_capabilities();
 
         require("fidget").setup({})
         require("mason").setup()
@@ -81,36 +67,6 @@ return {
                     lspconfig.volar.setup({})
                 end
             }
-        })
-
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ['<cr>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, { { name = 'buffer' } }),
-            -- sorting = {
-            --     comparators = {
-            --         cmp.config.compare.offset,
-            --         cmp.config.compare.exact,
-            --         cmp.config.compare.score,
-            --         cmp.config.compare.recently_used,
-            --         cmp.config.compare.kind,
-            --     },
-            -- },
         })
 
         vim.diagnostic.config({
