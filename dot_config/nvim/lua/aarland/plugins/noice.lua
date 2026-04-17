d6ac7e45f965fb5ac7093b7dbc2b4afc9a9d3525
Adrian Aarland <aaarland@reachmedianetwork.com>
2026-04-17 12:02:09 -0500

    feat: added hover for file:// uri in hover docs

--- dot_config/nvim/lua/aarland/plugins/noice.lua
+++ dot_config/nvim/lua/aarland/plugins/noice.lua
@@ -1,7 +1,29 @@
+local function open_with_position(uri)
+  pcall(vim.api.nvim_win_close, 0, true)
+
+  local decoded = vim.uri_decode(uri)
+
+  local file, fragment = decoded:match("file://([^#]+)#?(.*)")
+  file = vim.uri_to_fname("file://" .. file)
+
+
+  vim.cmd("edit " .. file)
+
+  if fragment then
+    local line, col = fragment:match("L(%d+),?(%d*)")
+    if line then
+      vim.api.nvim_win_set_cursor(0, {
+        tonumber(line),
+        tonumber(col ~= "" and col or 0)
+      })
+    end
+  end
+end
 return {
     "folke/noice.nvim",
     event = "VeryLazy",
     config = function()
+        local noiceOpen = require("noice.util")
         require("noice").setup({
             lsp = {
                 -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
@@ -10,6 +32,7 @@ return {
                     ["vim.lsp.util.stylize_markdown"] = true,
                     -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
                 },
+
             },
             -- you can enable a preset for easier configuration
             presets = {
@@ -18,6 +41,17 @@ return {
                 long_message_to_split = true, -- long messages will be sent to a split
                 inc_rename = false,           -- enables an input dialog for inc-rename.nvim
                 lsp_doc_border = true,        -- add a border to hover docs and signature help
+            },
+            markdown = {
+                hover = {
+                    ["%[.-%]%((%S-)%)"] = function(uri)
+                        if uri:match("^file://") then
+                                open_with_position(uri)
+                            return
+                        end
+                        return noiceOpen(uri)
+                    end
+                }
             },
             views = {
                 cmdline_popup = {
